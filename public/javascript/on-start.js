cb39580a77b3dd5a322e851aaff91d6c5674d937
johngiorgi <johnmgiorgi@gmail.com>
2018-12-16 19:11:35 -0500

    :recycle: Move declaration of FAB and tooltips to on-start

--- public/javascript/on-start.js
+++ public/javascript/on-start.js
@@ -1,6 +1,4 @@
 const $ = require("jquery");
-require("materialize-css")
-
 /*
 
 */
@@ -9,11 +7,25 @@ function onStart() {
     let window_width = $(window).width;
     let window_height = $(window).height;
 
-    console.log("ready to fade");
-    // $("#front-page").fadeIn(600);
-    // $("#front-page").addClass("in");
-
     $("#front-page").show();
+
+    // Enable FAB
+    let fab = document.querySelectorAll('.fixed-action-btnm');
+    let fabInstances = M.FloatingActionButton.init(fab, {
+      // User must click to open the tray
+      // In the future, it would be better to disable this on mobile but enable it on desktop
+      hoverEnabled: false,
+    });
+
+    // Enable FAB Tooltips
+    let tooltips = document.querySelectorAll('.tooltipped');
+    let tooltipInstances = M.Tooltip.init(tooltips, {
+      // Makes the tooltip animations much snappier than the default
+      inDuration: 200,
+      outDuration: 150,
+      transitionMovement: 5,
+    });
+
 }
 
 module.exports.onStart = onStart;
