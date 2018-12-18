65dd2221f6e21c9fa60b526f5c66168c6cfaf381
johngiorgi <johnmgiorgi@gmail.com>
2018-12-17 20:01:24 -0500

    :bug: Disable FAB tooltips on mobile

--- public/javascript/on-start.js
+++ public/javascript/on-start.js
@@ -4,8 +4,8 @@ const $ = require("jquery");
 */
 
 function onStart() {
-    let window_width = $(window).width;
-    let window_height = $(window).height;
+    let windowHeight = $(window).height;
+    let windowWidth = $(window).width;
 
     $("#front-page").show();
 
@@ -17,15 +17,16 @@ function onStart() {
       hoverEnabled: false,
     });
 
-    // Enable FAB Tooltips
-    let tooltips = document.querySelectorAll('.tooltipped');
-    let tooltipInstances = M.Tooltip.init(tooltips, {
-      // Makes the tooltip animations much snappier than the default
-      inDuration: 200,
-      outDuration: 150,
-      transitionMovement: 5,
-    });
-
+    // Enable FAB Tooltips only if use is NOT on a mobile device
+    if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+      let tooltips = document.querySelectorAll('.tooltipped');
+      let tooltipInstances = M.Tooltip.init(tooltips, {
+        // Makes the tooltip animations much snappier than the default
+        inDuration: 200,
+        outDuration: 150,
+        transitionMovement: 5,
+      });
+    }
 }
 
 module.exports.onStart = onStart;
