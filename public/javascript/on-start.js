d288c698f6a4ebba7f85474696a8e1967092dcdd
johngiorgi <johnmgiorgi@gmail.com>
2018-12-18 00:09:36 -0500

    :sparkles: Allow hover to trigger FAB on desktop

--- public/javascript/on-start.js
+++ public/javascript/on-start.js
@@ -7,26 +7,26 @@ function onStart() {
     let windowHeight = $(window).height;
     let windowWidth = $(window).width;
 
+    // Flag for whether or not FAB can be activated by hover
+    let hoverEnabled = true;
+
     $("#front-page").show();
+
+    if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+      // Require click to activate FAB on mobile device
+      let hoverEnabled = false;
+      // Enable FAB Tooltips only if use is NOT on a mobile device
+      let tooltips = document.querySelectorAll('.tooltipped');
+      let tooltipInstances = M.Tooltip.init(tooltips);
+    }
 
     // Enable FAB
     let fab = document.querySelectorAll('.fixed-action-btnm');
     let fabInstances = M.FloatingActionButton.init(fab, {
       // User must click to open the tray
       // In the future, it would be better to disable this on mobile but enable it on desktop
-      hoverEnabled: false,
+      hoverEnabled: hoverEnabled,
     });
-
-    // Enable FAB Tooltips only if use is NOT on a mobile device
-    if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-      let tooltips = document.querySelectorAll('.tooltipped');
-      let tooltipInstances = M.Tooltip.init(tooltips, {
-        // Makes the tooltip animations much snappier than the default
-        inDuration: 200,
-        outDuration: 150,
-        transitionMovement: 5,
-      });
-    }
 }
 
 module.exports.onStart = onStart;
