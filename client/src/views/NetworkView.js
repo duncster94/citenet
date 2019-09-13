722269514a01bdc48f31a6d18a5f811efe851190
duncster94 <duncan.forster@mail.utoronto.ca>
2019-09-13 16:41:09 -0400

    Basic modal functionality added for network view

--- client/src/views/NetworkView.js
+++ client/src/views/NetworkView.js
@@ -1,23 +1,18 @@
 import React from "react"
 import * as d3 from "d3"
 
-import Grid from "@material-ui/core/Grid"
-
-import { makeStyles } from "@material-ui/core/styles"
-import { Popper } from "@material-ui/core"
-import { Paper } from "@material-ui/core"
-import Typography from "@material-ui/core/Typography"
-import Grow from "@material-ui/core/Grow"
-
-import { CSSTransition } from "react-transition-group"
-
+import NodePopover from "./NetworkViewPopover"
+import NodeModal from "./NetworkViewModal"
 import "./NetworkView.css"
 
 export default function NetworkView({ props }) {
 
   const svgRef = React.useRef(null)
+  
   const [popoverAnchorEl, setPopoverAnchorEl] = React.useState(null)
-  const [popoverPaperData, setPopoverPaperData] = React.useState(0)
+  const [paperData, setPaperData] = React.useState(0)
+
+  const [isModalOpen, setIsModalOpen] = React.useState(false)
 
   React.useEffect(() => {
 
@@ -45,7 +40,7 @@ export default function NetworkView({ props }) {
     const circles = nodes.append("circle")
       .attr("r", 10)
       .on("mouseover", function(data) {
-        setPopoverPaperData(data)
+        setPaperData(data)
         setPopoverAnchorEl(this)
       })
       .on("mouseout", () => {
@@ -53,6 +48,7 @@ export default function NetworkView({ props }) {
       })
       .on("click", function(data) {
         console.log(data)
+        setIsModalOpen(true)
       })
     
     nodes
@@ -109,140 +105,14 @@ export default function NetworkView({ props }) {
       <NodePopover 
         props={{
           popoverAnchorEl,
-          data: popoverPaperData
+          data: paperData
         }}
       />
+      <NodeModal props={{
+        isModalOpen,
+        setIsModalOpen,
+        data: paperData
+      }} />
     </svg>
   )
 }
-
-
-function NodePopover({ props }) {
-
-  const [arrowRef, setArrowRef] = React.useState(null)
-  const isOpen = Boolean(props.popoverAnchorEl)
-
-  return (
-    <CSSTransition 
-      in={isOpen} 
-      timeout={350} 
-      classNames="popper-transition"
-      unmountOnExit
-    >
-    <Popper
-      className="network-node-popover"
-      // className="popper"
-      className={useStyles().popper}
-      open={true}
-      anchorEl={props.popoverAnchorEl}
-      placement="top"
-      // disablePortal={false}
-      modifiers={{
-            arrow: {
-              enabled: true,
-              element: arrowRef
-          }
-        }}
-    > 
-      <Grow timeout={350} in={isOpen}>
-      <Paper>
-      <span className={useStyles().arrow} ref={setArrowRef}/>
-        <Grid
-          className="network-node-popover-grid"
-          direction="column"
-          alignItems="center"
-          container
-        >
-          <Grid item xs>
-            <Typography
-              align="center"
-              color="textPrimary"
-              variant="body1"
-              gutterBottom
-            >
-              {props.data.title}
-            </Typography>
-          </Grid>
-
-          <Grid item xs>
-            <Typography 
-              align="center" 
-              color="textSecondary" 
-              variant="body2"
-              gutterBottom
-            >
-              authors (need to format)
-            </Typography>
-          </Grid>
-
-        </Grid>
-      </Paper>
-      </Grow>
-    </Popper>
-      </CSSTransition>
-  )
-}
-
-
-const useStyles = makeStyles(theme => ({
-  popper: {
-    zIndex: 1,
-    '&[x-placement*="bottom"] $arrow': {
-      top: 0,
-      left: 0,
-      marginTop: '-0.9em',
-      width: '3em',
-      height: '1em',
-      '&::before': {
-        borderWidth: '0 1em 1em 1em',
-        borderColor: `transparent transparent ${theme.palette.background.paper} transparent`,
-      },
-    },
-    '&[x-placement*="top"] $arrow': {
-      bottom: 0,
-      left: 0,
-      marginBottom: '-0.9em',
-      width: '3em',
-      height: '1em',
-      '&::before': {
-        borderWidth: '1em 1em 0 1em',
-        borderColor: `${theme.palette.background.paper} transparent transparent transparent`,
-      },
-    },
-    '&[x-placement*="right"] $arrow': {
-      left: 0,
-      marginLeft: '-0.9em',
-      height: '3em',
-      width: '1em',
-      '&::before': {
-        borderWidth: '1em 1em 1em 0',
-        borderColor: `transparent ${theme.palette.background.paper} transparent transparent`,
-      },
-    },
-    '&[x-placement*="left"] $arrow': {
-      right: 0,
-      marginRight: '-0.9em',
-      height: '3em',
-      width: '1em',
-      '&::before': {
-        borderWidth: '1em 0 1em 1em',
-        borderColor: `transparent transparent transparent ${theme.palette.background.paper}`,
-      },
-    },
-  },
-  arrow: {
-    position: 'absolute',
-    fontSize: 7,
-    width: '3em',
-    height: '3em',
-    pointerEvents: "none",
-    '&::before': {
-      content: '""',
-      margin: 'auto',
-      display: 'block',
-      width: 0,
-      height: 0,
-      borderStyle: 'solid',
-    },
-  },
-}))
