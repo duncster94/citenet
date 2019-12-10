f79e4eda5343048e181466d0e163068000da6815
duncster94 <duncan.forster@mail.utoronto.ca>
2019-12-10 15:26:33 -0500

    switched homepage css to makestyles and shifted content up slightly

--- client/src/home-page/HomePage.js
+++ client/src/home-page/HomePage.js
@@ -1,16 +1,43 @@
 import React from "react"
 import Grid from "@material-ui/core/Grid"
 import Typography from "@material-ui/core/Typography"
+import { makeStyles } from "@material-ui/core/styles"
 
 import theme from "../Theme"
-import "./HomePage.css"
 import SearchBar from "./SearchBar"
+
+const useStyles = makeStyles({
+  root: {
+    position: "absolute",
+    flexGrow: 1,
+    display: "flex",
+    height: "inherit",
+    width: "inherit",
+  },
+  search: {
+    marginTop: "10px",
+    marginBottom: "10px",
+  },
+  logo: {
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
+    maxWidth: "50%",
+    height: "auto",
+  },
+  // tagline: {
+  //   color: theme.palette.primary.main,
+  //   marginBottom: "15vh"
+  // }
+})
 
 export default function HomePage({ props }) {
 
+  const classes = useStyles()
+
   return (
     <React.Fragment>
-      <div className="homepage-root">
+      <div className={classes.root}>
         <Grid
           container
           direction="column"
@@ -24,7 +51,7 @@ export default function HomePage({ props }) {
 
           <Grid
             container
-            className="homepage-search"
+            className={classes.search}
             justify="center"
             alignItems="center"
           >
@@ -37,7 +64,12 @@ export default function HomePage({ props }) {
           </Grid>
 
           <Grid item>
-            <Typography style={{color: theme.palette.primary.main}}>
+            <Typography 
+              style={{
+                color: theme.palette.primary.main,
+                marginBottom: "15vh"
+              }} // This should be in `makeStyles` but it doesn't work...?
+            >
               A powerfully intuitive way to search the scientific literature
             </Typography>
           </Grid>
@@ -49,9 +81,12 @@ export default function HomePage({ props }) {
 }
 
 function Logo() {
+
+  const classes = useStyles()
+
   return (
     <img
-      className="homepage-logo"
+      className={classes.logo}
       alt="CiteNet logo"
       src="/images/citenet_logo.png"
     />
