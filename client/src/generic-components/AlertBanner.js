2ffbdbfe9f60e3c0b5162e85b8bbb10146a239a7
duncster94 <duncan.forster@mail.utoronto.ca>
2020-03-15 19:54:49 -0400

    fixed banner
    Co-authored-by: John Giorgi <johnmgiorgi@gmail.com>

--- client/src/generic-components/AlertBanner.js
+++ client/src/generic-components/AlertBanner.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import Container from '@material-ui/core/Container'
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
-import Link from '@material-ui/core/Link'
+import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
 import { makeStyles } from '@material-ui/core/styles'
+import Icon from "@mdi/react"
+import { mdiClose } from "@mdi/js"
+import theme from '../Theme'
 
 const MESSAGE = `CiteNet is currently in development. However, due to the COVID-19 pandemic we have opted
 to: 1. Make CiteNet available to the public and 2. Index bioRxiv and medRxiv on a nightly basis. We hope to provide
@@ -26,21 +28,45 @@ const useStyles = makeStyles(theme => ({
 }))
 
 const AlertBanner = () => {
-  const classes = useStyles()
+  const [open, setOpen] = React.useState(true);
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return
+    }
+    setOpen(false)
+  }
 
   return (
-    <Container className={classes.root}>
-      <Card className={classes.card}>
-        <CardContent>
-          <Typography gutterBottom>
+    <div>
+      <Snackbar
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "center"
+        }}
+        open={open}
+        onClose={handleClose}
+      >
+        <Paper style={{ backgroundColor: theme.palette.secondary.main, padding: "10px" }}>
+          <div style={{display: 'flex'}}>
+          <Typography style={{color: '#fff'}}>
             {MESSAGE}
           </Typography>
-          <Link href="/covid-disclaimer" className={classes.link} underline="always">
-            Please read our disclaimer.
-          </Link>
-        </CardContent>
-      </Card>
-    </Container>
+          <IconButton
+            size="small"
+            aria-label="close"
+            onClick={handleClose}
+            style={{
+              height: '30px',
+              width: '30px'
+            }}
+          >
+            <Icon path={mdiClose} size={1} color="#fff"/>
+          </IconButton>
+          </div>
+        </Paper>
+      </Snackbar>
+    </div>
   )
 }
 
